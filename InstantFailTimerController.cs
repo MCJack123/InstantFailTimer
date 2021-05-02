698eba587f51fb31a30117664c5367b8f8d881f2
MCJack123 <jackmacwindowslinux@gmail.com>
2021-05-02 03:45:24 -0400

    Add project files.

---
+++ InstantFailTimerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using UnityEngine;
+using BS_Utils.Utilities;
+
+namespace InstantFailTimer {
+    /// <summary>
+    /// Monobehaviours (scripts) are added to GameObjects.
+    /// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
+    /// </summary>
+    public class InstantFailTimerController : MonoBehaviour {
+        public static InstantFailTimerController Instance { get; private set; }
+
+        public void OnGameSceneActive() {
+            BSEvents.noteWasMissed += OnNoteWasMissed;
+            BSEvents.levelCleared += OnLevelCleared;
+        }
+
+        private void OnNoteWasMissed(NoteData data, int score) {
+            Plugin.Log.Debug(data.time.ToString());
+            if (data.time < Configuration.PluginConfig.Instance.failTime) {
+                Resources.FindObjectsOfTypeAll<StandardLevelGameplayManager>().LastOrDefault()?.HandleGameEnergyDidReach0();
+                Resources.FindObjectsOfTypeAll<MissionLevelGameplayManager>().LastOrDefault()?.HandleGameEnergyDidReach0();
+            }
+            BSEvents.noteWasMissed -= OnNoteWasMissed;
+        }
+
+        private void OnLevelCleared(StandardLevelScenesTransitionSetupDataSO data, LevelCompletionResults res) {
+            BSEvents.noteWasMissed -= OnNoteWasMissed;
+            BSEvents.levelCleared -= OnLevelCleared;
+        }
+
+        // These methods are automatically called by Unity, you should remove any you aren't using.
+        #region Monobehaviour Messages
+        /// <summary>
+        /// Only ever called once, mainly used to initialize variables.
+        /// </summary>
+        private void Awake() {
+            // For this particular MonoBehaviour, we only want one instance to exist at any time, so store a reference to it in a static property
+            //   and destroy any that are created while one already exists.
+            if (Instance != null) {
+                Plugin.Log?.Warn($"Instance of {GetType().Name} already exists, destroying.");
+                GameObject.DestroyImmediate(this);
+                return;
+            }
+            GameObject.DontDestroyOnLoad(this); // Don't destroy this object on scene changes
+            Instance = this;
+            Plugin.Log?.Debug($"{name}: Awake()");
+        }
+
+        /// <summary>
+        /// Called when the script is being destroyed.
+        /// </summary>
+        private void OnDestroy() {
+            Plugin.Log?.Debug($"{name}: OnDestroy()");
+            if (Instance == this)
+                Instance = null; // This MonoBehaviour is being destroyed, so set the static instance property to null.
+
+        }
+        #endregion
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,60 @@
+﻿using IPA;
+using IPA.Config.Stores;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+using BS_Utils.Utilities;
+
+namespace InstantFailTimer {
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin {
+        // TODO: If using Harmony, uncomment and change YourGitHub to the name of your GitHub account, or use the form "com.company.project.product"
+        //       You must also add a reference to the Harmony assembly in the Libs folder.
+        // public const string HarmonyId = "com.github.YourGitHub.InstantFailTimer";
+        // internal static readonly HarmonyLib.Harmony harmony = new HarmonyLib.Harmony(HarmonyId);
+
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+        internal static InstantFailTimerController PluginController { get { return InstantFailTimerController.Instance; } }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public Plugin(IPALogger logger) {
+            Instance = this;
+            Plugin.Log = logger;
+            Plugin.Log?.Debug("Logger initialized.");
+        }
+
+        [Init]
+        public void InitWithConfig(IPA.Config.Config conf)
+        {
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            Plugin.Log?.Debug("Config loaded");
+        }
+
+
+        /// <summary>
+        /// Called when the plugin is enabled (including when the game starts if the plugin is enabled).
+        /// </summary>
+        [OnEnable]
+        public void OnEnable() {
+            new GameObject("InstantFailTimerController").AddComponent<InstantFailTimerController>();
+            BSEvents.gameSceneActive += PluginController.OnGameSceneActive;
+        }
+
+        /// <summary>
+        /// Called when the plugin is disabled and on Beat Saber quit. It is important to clean up any Harmony patches, GameObjects, and Monobehaviours here.
+        /// The game should be left in a state as if the plugin was never started.
+        /// Methods marked [OnDisable] must return void or Task.
+        /// </summary>
+        [OnDisable]
+        public void OnDisable() {
+            if (PluginController != null)
+                GameObject.Destroy(PluginController);
+            BSEvents.gameSceneActive -= PluginController.OnGameSceneActive;
+        }
+    }
+}
